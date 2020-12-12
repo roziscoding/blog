e3b7e9684c166a64a0a3eca0eb16ae050fb78902
Rogério Munhoz <roz@rjmunhoz.me>
2020-12-11 21:09:42 -0300

    Initial commit

---
+++ blog/.vuepress/config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  title: 'Roz Is Coding', // Title for the site. This will be displayed in the navbar.
+  theme: '@vuepress/theme-blog',
+  themeConfig: {
+    footer: {
+      contact: [
+        {
+          type: 'github',
+          link: 'https://roz.ninja/github?ref=blog',
+        },
+        {
+          type: 'linkedin',
+          link: 'https://roz.ninja/linkedin?ref=blog',
+        },
+        {
+          type: 'twitter',
+          link: 'https://roz.ninja/twitter?ref=blog',
+        },
+      ],
+    },
+    comment: {
+      service: 'vssue',
+      owner: 'roziscoding',
+      repo: 'blog',
+      clientId: '95dd788d52190db920bd',
+      clientSecret: '3f392c443e74114e88adc03736a5d03ff598b5ed',
+    },
+    smoothScroll: true,
+  },
+};
---
+++ blog/_posts/2020-12-11-hello-world.md
@@ -0,0 +1,17 @@
+---
+title: Hello World
+date: 2020-12-11
+author: roziscoding
+location: São Paulo
+tags:
+  - off-topic
+summary: Habemus blog!
+---
+
+Habemus blog!
+
+Fazia um tempo que eu tava pra criar meu blog.
+
+Logo logo vem artigo dahora por aí.
+
+Provavelmente vai ter TypeScript, JavaScript, Node e afins.
